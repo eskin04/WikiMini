289f0c26265eca673e8ff90d86e047b9a7ea9877
okan eskin <71434594+eskin04@users.noreply.github.com>
2023-09-05 11:07:53 +0300

    push wikiMini

---
+++ WikiMini/src/app/[searchTerm]/components/item.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {
+  result: Result;
+};
+
+function Item({ result }: Props) {
+  const itemTextCol = (
+    <div className="flex flex-col justify-center">
+      <h2 className=" text-xl font-bold underline">
+        <Link
+          href={`https://tr.wikipedia.org/wiki/${result.title}`}
+          target="_blank"
+        >
+          {result.title}
+        </Link>
+      </h2>
+      <p>{result.extract}</p>
+    </div>
+  );
+  const content = result.thumbnail?.source ? (
+    <article className="m-4 max-w-lg">
+      <div className="flex flex-row gap-4">
+        <div className="flex flex-col justify-center">
+          <img
+            src={result.thumbnail.source}
+            width={result.thumbnail.width}
+            height={result.thumbnail.height}
+            alt={result.title}
+            loading="lazy"
+          />
+        </div>
+        {itemTextCol}
+      </div>
+    </article>
+  ) : (
+    <article className="m-4 max-w-lg">{itemTextCol}</article>
+  );
+  return content;
+}
+
+export default Item;
